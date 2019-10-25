64e062bd92589bde5115fae369fec601c0684523
Lin2D2 <linus@thebehrens.net>
2019-10-25 16:41:28 +0200

    initial commit

---
+++ libresign2/infoscreen/infoscreen.py
@@ -0,0 +1,110 @@
+import os, time, sys, logging
+from PIL import ImageTk
+from PIL import Image as Image_
+import tkinter as tk
+import qrcode
+
+bg_color = "#55A555"
+
+
+class InfoScreen(tk.Frame):
+    def __init__(self, master=None, url=None):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.state = False
+        self.master.bind("<Escape>", self.end_fullscreen)
+        self.pack(fill=tk.BOTH, expand=1)
+        self.url = url
+        self.setup()
+
+        # TODO add listener here for event in unoremote
+    def toggle_fullscreen(self, event=None, state=None, mode=0):
+        print("this event is run l.244 unoremote level 5")
+        if state is None:
+            self.state = not self.state
+        else:
+            self.state = state
+        self.master.attributes('-topmost', self.state)
+        if mode == 1:
+            self.master.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.master.attributes("-fullscreen", self.state)
+        self.master.attributes('-topmost', self.state)
+        return "break"
+
+    def setup (self):
+        font        = ('Helvetica', 30)
+        smallfont   = ('Helvetica', 20)
+        # NOTE text height = 50 is somewhat arbitrary
+        height      = '50'
+
+        self.msg_txt = tk.Label(self.master)
+        self.msg_txt['text'] = \
+            'Visit the URL below to reach the control panel.'
+        self.msg_txt.configure(foreground='white',
+                               font=font, background=bg_color)
+        self.msg_txt.place(relx='0.5', rely='0.4', anchor='center',
+                           height=height)
+
+        # show the URL to which the end-users should connect to reach
+        # the control panel
+
+        self.url_text = tk.Label(self.master)
+        self.url_text["text"] = self.url
+        self.url_text.configure(background=bg_color, foreground='white',
+                                font=font)
+        self.url_text.place(relx='0.5', rely='0.5', anchor='center',
+                            height=height)
+
+        # Qr Code !!!
+
+        imagepath = os.getcwd()
+
+        qr = qrcode.QRCode(
+            version=5,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=6,
+            border=8
+        )
+        qr.add_data(self.url)
+        qr.make()
+        img = qr.make_image(fill_color="black", back_color=bg_color)
+        img.save(imagepath + '/data_png.png')
+
+        load = Image_.open(imagepath + '/data_png.png')
+        render = ImageTk.PhotoImage(load)
+        qrcode_ = tk.Label(self, image=render)
+        qrcode_.image = render
+        qrcode_.place(relx='0.5', rely='0.7', anchor='center')
+
+        # link to code repo
+        code_site = tk.Label(self.master)
+        code_site['text'] = 'Get the code at: https://github.com/LibreOffice/libresign'
+        code_site.configure(background=bg_color, foreground='white',
+                                font=smallfont)
+        code_site.place(relx='0', rely='1.0', anchor='sw',
+                            height=height)
+
+        copyright = tk.Label(self.master)
+        copyright['text'] = 'The Document Foundation'
+        copyright.configure(background=bg_color, foreground='white',
+                                font=smallfont)
+        copyright.place(relx='1.0', rely='1.0', anchor='se',
+                            height=height)
+
+
+def start_info_screen(url):
+    root = tk.Tk()
+    root.wm_title("Tkinter window")
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    root.geometry("%dx%d+0+0" % (w, h))
+    root.attributes('-fullscreen', True)
+    # root.attributes('-topmost', True)
+
+    app = InfoScreen(master=root, url=url)
+    app.configure(background=bg_color)
+    root.mainloop()
---
+++ libresign2/main.py
@@ -0,0 +1,127 @@
+import time, logging, os, sys, multiprocessing, subprocess
+import json
+import libresign2.infoscreen.infoscreen as infoscreen
+
+
+class LibresignInstance():
+    def __init__(self):
+        self.settings_path = "settings.json"
+        self.home_dir = self.read_settings("HomeDir")
+        self.infoscreen_process = None
+        self.network_addresse = None
+
+    def read_settings(self, parameter):
+        with open(self.settings_path, "r") as json_file:
+            data = json.load(json_file)
+            try:
+                # add that you can do more then just reading one parameter at a time
+                if type(parameter) == list:
+                    result = []
+                    for p in parameter:
+                        result.append(data[p])
+                    return result
+                elif type(parameter) == str:
+                    return data[parameter]
+                else:
+                    logging.warning(['read_settings, return None', [parameter, type(parameter)]])
+                    return None
+            except:
+                logging.exception(["Unexpected error at reading settings:", sys.exc_info()[0], parameter])
+
+    def write_settings(self, parameter, value):
+        with open(self.settings_path, "r") as json_file:
+            data = json.load(json_file)
+        with open(self.settings_path, "w") as json_file:
+            try:
+                # add that you can do more then just write more than one parameter
+                if type(parameter) == list and type(value) == list:
+                    if len(parameter) == len(value):
+                        for p, v in parameter, value:
+                            data[p] = v
+                        json.dump(data, json_file)
+                        logging.debug(["successful wrote", value, "to", parameter])
+                elif type(parameter) == str:
+                    data[parameter] = value
+                    json.dump(data, json_file)
+                    logging.debug(["successfully wrote", value, "to", parameter])
+                else:
+                    logging.warning(['write_settings didn\'t wrote', [parameter, type(parameter)], [value, type(value)]])
+            except:
+                logging.exception(["Unexpected error at writing settings:", sys.exc_info()[0], parameter, value])
+
+    # TODO return True or False and logging.info()
+    # TODO for now set to True
+    def network_connection(self):
+        return True
+
+    def retry_network_connection(self):
+        while not self.network_connection():
+            logging.warning(["no network connection"])
+            time.sleep(2)
+
+    def run(self):
+        if not self.network_connection():
+            self.retry_network_connection()
+
+        # TODO for now set to some url
+        url = "http://google.com"
+        self.infoscreen_process = multiprocessing.Process(target=infoscreen.start_info_screen, args=(url,))
+        try:
+            self.infoscreen_process.start()
+            logging.info(["Infoscreen started"])
+        except:
+            logging.warning(["Infoscreen not started"])
+
+        # TODO start Control sever
+
+        cwd = os.getcwd()
+        os.chdir(self.home_dir + '/web_control_panel')
+        args = ['python3', '-m', 'http.server', self.read_settings("HTTP_PORT")]
+        subprocess.Popen(args)
+
+        os.chdir(cwd)
+
+        # TODO start LibreOffice Instance
+
+        # TODO add an option to quit to Program
+
+
+def setup():
+    args = sys.argv
+    settings_to_write_parameter = []
+    settings_to_write_value = []
+    for i in range(len(args)):
+        arg = args[i]
+
+        # don't show the fullscreen info screen
+        if arg == '--noinfo':
+            settings_to_write_parameter.append("SHOW_INFO_SCREEN")
+            settings_to_write_value.append(False)
+
+        # default anyway
+        if arg == '--conference':
+            settings_to_write_parameter.append("CONFERENCE")
+            settings_to_write_value.append(True)
+
+        # change home dir
+        if arg == '--libresign-home':
+            if os.path.isdir(args[i + 1]):
+                settings_to_write_parameter.append("HomeDir")
+                settings_to_write_value.append(args[i + 1])
+                i += 1
+                logging.info('libresign home', settings_to_write_value[-1])
+            else:
+                logging.warning('libresign home', settings_to_write_value[-1])
+
+    libresign_instance = LibresignInstance()
+    logging_level = libresign_instance.read_settings('LOGGING_LEVEL')
+    logging.root.setLevel(logging_level)
+    logging.info(['Libresign Instance created', 'sys.args=', args[1:]])
+    libresign_instance.write_settings("HomeDir", os.path.dirname(os.path.realpath(__file__)))
+    # TODO write settings from above to settings.json
+    # libresign_instance.write_settings(settings_to_write_parameter, settings_to_write_value)
+    del settings_to_write_parameter, settings_to_write_value
+    logging.info(["Setup completed", libresign_instance])
+    libresign_instance.run()
+
+setup()
