ae8469b7be7c9cdb391ac4b71e921a50ec7066bb
Linus Behrens <linus@thebehrens.net>
2019-10-31 12:34:55 +0100

    added more comandline arguments an ....

--- setup.sh
+++ setup.sh
@@ -5,6 +5,20 @@ args=("$@")
 
 echo runn with ${args[0]}
 
+if [[ ${args[1]} = "--help" ]]; then
+    echo "--setup   to start setup"
+    echo "--ignore if you want to not quit if the requirements are not satisfied"
+    echo "    you may have to do this because the paths to check if th requirements are satisfied are not on all systems the same"
+    echo ""
+    echo "  more feathers are planned"
+    exit
+fi
+
+if [[ ${args[1]} = "--setup" ]]; then
+    echo "you are not in setup mode. In order to start setup: --setup or --help for more arguments"
+    exit
+fi
+
 # TODO "apt-get install virtualenv python3-xdo python3-tk"
 echo you have to run "apt-get install virtualenv python3-xdo python3-tk python3-uno"
 REQUIERMENTS="True"
@@ -29,7 +43,7 @@ if [[ ! -d "/usr/share/doc/python3-uno" ]]; then
     REQUIERMENTS="False"
 fi
 
-if [[ "$REQUIERMENTS" = "False" ]] || [[ ${args[0]} = "-i" ]]; then
+if [[ "$REQUIERMENTS" = "False" ]] || [[ ${args[1]} = "--ignore" ]]; then
     echo requirments not satisfied, quiting...
     exit
 fi
